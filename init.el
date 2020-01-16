0be4701ff7ed7c03f768c7743e25c64773fe757f
ryn1x <ryangoodner@protonmail.com>
2020-01-15 18:51:14 -0700

    update font

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 120 :width normal)))))
 
 ;; rustfmt on save
 (setq rust-format-on-save t)
