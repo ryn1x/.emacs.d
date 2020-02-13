abe2d49a2c3a35d750584da137ee8d30e97f8cd9
ryn1x <ryangoodner@protonmail.com>
2020-02-12 23:52:24 -0700

    update packages

---
+++ elpa/fish-mode-20200210.1815/fish-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fish-mode" "20200210.1815" "Major mode for fish shell scripts" '((emacs "24")) :commit "83b21fab1d9863dc91b5b871ac45c3363237683a" :keywords '("fish" "shell") :authors '(("Tony Wang" . "wwwjfy@gmail.com")) :maintainer '("Tony Wang" . "wwwjfy@gmail.com"))
---
+++ elpa/geiser-20200213.355/geiser-pkg.el
@@ -0,0 +1,4 @@
+(define-package "geiser" "20200213.355" "GNU Emacs and Scheme talk to each other" 'nil :url "http://www.nongnu.org/geiser/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
